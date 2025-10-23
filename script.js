3d495eaab74eb8777692fa044bef97f87369b934
ogozalvez <ogozalvez@cendrassos.net>
2025-10-23 12:24:35 +0200

    Arreglo del menú toggle

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 // Aquí podrás añadir efectos visuales, animaciones o interacciones
 console.log("Bienvenido a ogozalvez.cat");
-
 console.log("Página de colección cargada");
 
 // Menú hamburguesa
@@ -10,4 +9,5 @@ document.addEventListener("DOMContentLoaded", () => {
 
   toggle.addEventListener("click", () => {
     menu.classList.toggle("active");
-  }
+  });
+});
